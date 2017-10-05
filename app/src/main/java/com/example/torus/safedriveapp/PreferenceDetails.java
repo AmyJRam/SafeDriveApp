4b98effe7e3153c692fa5f1aaa61c0f8c2431566
Amrutha <amruthajayaram19@gmail.com>
2017-10-05 18:48:44 +0530

    After Adding Preference Details

---
+++ app/src/main/java/com/example/torus/safedriveapp/PreferenceDetails.java
@@ -0,0 +1,15 @@
+package com.example.torus.safedriveapp;
+
+/**
+ * Created by TORUS on 10/5/2017.
+ */
+
+public class PreferenceDetails {
+    String guardian_name_key="Guardian";
+    String guardian_phone_key="GuardianPhone";
+    String speed_key="Speed";
+    String check_safety_key="CheckSafety";
+    String sms_key="SMS";
+    String guardian_name,getGuardian_phone_key,speed,check_safety;
+
+}
--- app/src/main/java/com/example/torus/safedriveapp/SqlHelper.java
+++
@@ -1,8 +0,0 @@
-package com.example.torus.safedriveapp;
-
-/**
- * Created by TORUS on 10/5/2017.
- */
-
-public class SqlHelper {
-}
