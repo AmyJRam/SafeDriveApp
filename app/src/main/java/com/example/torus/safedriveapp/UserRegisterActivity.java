0a68b14ba570752df332881cf1c0dbee32029289
Amrutha <amruthajayaram19@gmail.com>
2017-10-05 14:46:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/torus/safedriveapp/UserRegisterActivity.java
@@ -0,0 +1,17 @@
+package com.example.torus.safedriveapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by TORUS on 10/5/2017.
+ */
+
+public class UserRegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_register_layout);
+    }
+}
